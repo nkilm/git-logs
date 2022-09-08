e9ef0db5eee2b779ab6103af751162d8ab5911cf
Nikhil Mohite <nikhilmohitelhs@gmail.com>
2022-09-08 19:19:36 +0530

    add args

--- gitstats.py
+++ gitstats.py
@@ -4,7 +4,7 @@ import subprocess
 
 
 def is_not_git():
-    """ Check if git is initialized in the repository """
+    """Check if git is initialized in the repository"""
     try:
         current_path = Path(__file__).parent.resolve()
         _ = git.Repo(current_path).git_dir
@@ -13,21 +13,33 @@ def is_not_git():
         return True
 
 
-def get_logs():
+def get_logs(before="", after="", reverse=""):
     """Return results of git log [args]"""
 
     # < day, date, author >
-    args = ["git", "log", '--pretty=format:%aD,%aN']
+    args = ["git", "log", "--pretty=format:%aD,%aN"]
 
-    commit_logs = [] 
+    commit_logs = []
     try:
-        logs = subprocess.check_output(args,shell=False,universal_newlines=True).split("\n")
+        if after:
+            args.append("--after=%s" % (after,))
+        if before:
+            args.append("--before=%s" % (before,))
+
+        logs = subprocess.check_output(
+            args, shell=False, universal_newlines=True
+        ).split("\n")
 
         # Process each line
         for line in logs:
-            day,time_stamp,author = line.split(",")
+            day, time_stamp, author = line.split(",")
             time_stamp = f"{day} -{time_stamp}"
-            commit_logs.append({"time_stamp":time_stamp,"author":author})
+            commit_logs.append({"time_stamp": time_stamp, "author": author})
+
+        if(reverse):
+            return commit_logs.reverse()
+            
+        return commit_logs
 
     except Exception as e:
         print(e)
