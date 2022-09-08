e2b634038d16c6b325de0a411fa1634fc56a3e2f
Nikhil Mohite <nikhilmohitelhs@gmail.com>
2022-09-08 20:24:02 +0530

    add frequency

--- parser.py
+++ parser.py
@@ -1,25 +1,25 @@
 import argparse
 
-p = argparse.ArgumentParser(description="Display information from .git")
+args_parser = argparse.ArgumentParser(description="Display information from .git")
 
-p.add_argument("-p", "--periodicity", action="store", dest="periodicity",
+args_parser.add_argument("-f", "--frequency", action="store", dest="frequency",
                    type=str, required=False, default="month",
                    choices=["day", "week", "month", "year"])
 
-p.add_argument("-u", "--author", action="store", dest="author",
+args_parser.add_argument("-u", "--author", action="store", dest="author",
                    type=str, required=False, default="",
                    help="filter by author's e-mail (substring)")
 
-p.add_argument("-a", "--after", action="store", dest="after",
+args_parser.add_argument("-a", "--after", action="store", dest="after",
                    type=str, required=False, default="",
                    help="after date (yyyy-mm-dd hh:mm)")
 
-p.add_argument("-b", "--before", action="store", dest="before",
+args_parser.add_argument("-b", "--before", action="store", dest="before",
                    type=str, required=False, default="",
                    help="before date (yyyy-mm-dd hh:mm)")
 
-p.add_argument("-r", "--reverse", action="store", dest="reverse",
+args_parser.add_argument("-r", "--reverse", action="store", dest="reverse",
                    type=bool, required=False, default=False,
                    help="reverse date order")
-    
-args = p.parse_args()
+
+args = args_parser.parse_args()
