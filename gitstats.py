0301b3ba8efad86a49e271e5764cb498ef1e4696
Nikhil Mohite <nikhilmohitelhs@gmail.com>
2022-09-09 00:09:25 +0530

    display normalized logs

--- gitstats.py
+++ gitstats.py
@@ -1,9 +1,9 @@
 import git
-import sys
 import subprocess
 from pathlib import Path
 from collections import OrderedDict
 from datetime import datetime
+from utils.bcolors import bcolors
 
 
 def is_not_git() -> bool:
@@ -16,7 +16,7 @@ def is_not_git() -> bool:
         return True
 
 
-def get_logs(before="", after="", reverse="") -> list:
+def get_logs(before: str, after: str, reverse: bool) -> list:
     """Return results of git log [args]"""
 
     # < day, date, author >
@@ -24,10 +24,10 @@ def get_logs(before="", after="", reverse="") -> list:
 
     commit_logs = []
     try:
-        if after:
-            args.append("--after=%s" % (after,))
+        if after!="":
+            args.append("--after="+after)
         if before:
-            args.append("--before=%s" % (before,))
+            args.append("--before="+before)
 
         logs = subprocess.check_output(
             args, shell=False, universal_newlines=True
@@ -56,7 +56,6 @@ def filter_logs(logs: list, author: str, frequency="day") -> dict:
         commit_date = commit_info["time_stamp"].split(" ")[0]
 
         is_weekend = False
-
         if frequency == "week":
             # %V gives number of weeks so far in a given year
             commit_date = datetime.strptime(commit_date, "%Y-%m-%d").strftime("%Y/%V")
@@ -66,7 +65,11 @@ def filter_logs(logs: list, author: str, frequency="day") -> dict:
             commit_date = commit_info["time_stamp"][:4]  # YYYY
         else:
             # return the day of the week
-            is_weekend = datetime.strptime(commit_date, "%Y-%m-%d").weekday() > 4
+            is_weekend = (
+                True
+                if (datetime.strptime(commit_date, "%Y-%m-%d").weekday() > 4)
+                else False
+            )
             # Monday=0, Tuesday=1, Wednesday=2 and so on..
 
         if author != "":
@@ -87,23 +90,38 @@ def filter_logs(logs: list, author: str, frequency="day") -> dict:
 
 
 def normalize(value: int, xmin: int, xmax: int) -> float:
-    """ Return min-max normalized value """
+    """Return min-max normalized value"""
     return float(value - xmin) / float(xmax - xmin)
+
 
 def get_relative_count(filtered_commits: OrderedDict) -> OrderedDict:
     """Compute normalized score/count based on given filter"""
 
     values = [item["commits"] for item in filtered_commits.values()]
-    values.append(0) # To handle the case where only 1 value is present
+    values.append(0)  # To handle the case where only 1 value is present
 
     xmin = min(values)
     xmax = max(values)
 
     normalized_info = OrderedDict()
-    
+
     for commit_date in filtered_commits.keys():
         normalized_info[commit_date] = filtered_commits[commit_date].copy()
-        # add normalized value 
-        normalized_info[commit_date]["score"] = normalize(filtered_commits[commit_date]["commits"], xmin, xmax)
+        # add normalized value
+        normalized_info[commit_date]["score"] = normalize(
+            filtered_commits[commit_date]["commits"], xmin, xmax
+        )
 
     return normalized_info
+
+
+def display(logs: OrderedDict) -> None:
+    """Display commit information to stdout"""
+
+    for commit_date in logs:
+        count = str(logs[commit_date]["commits"])
+
+        print(f"{bcolors.header(commit_date)}  {bcolors.okblue(count)}", end="\t")
+
+        # sys.stdout.write("-" * int(logs[commit_date]["score"] * 50) + "\n")
+        print(bcolors.ok("-") * int(logs[commit_date]["score"] * 50))
--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-from gitstats import is_not_git, get_logs, filter_logs, get_relative_count
+from dis import dis
+from gitstats import display, is_not_git, get_logs, filter_logs, get_relative_count
 from parser import args
+from validate.check_args import validate
 
 
 def main():
     if is_not_git():
         print('Git is not initialized. Please initialize using "git init"')
 
-    """Program start"""
+    """ Check if correct Arguments given """
+
+    all_correct = validate(
+        args.before, args.after, args.frequency, args.author, args.reverse
+    )
+    if not all_correct:
+        exit(1)
+
+    """ Program start """
     logs = []
     try:
-        logs = get_logs(args.after, args.before, args.reverse)
+        logs = get_logs(args.before, args.after, args.reverse)
     except Exception as e:
         print(f"error running 'git log': {e}")
         exit(1)
 
     filtered = filter_logs(logs, args.author, args.frequency)
     normalized_logs = get_relative_count(filtered)
+
+    display(normalized_logs)
 
 
 if __name__ == "__main__":
