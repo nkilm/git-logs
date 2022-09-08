38a12453274e1ec600a4c7a7593709b4fc9af841
Nikhil Mohite <nikhilmohitelhs@gmail.com>
2022-09-08 21:58:06 +0530

    list commits based on frequency and filter by author

--- gitstats.py
+++ gitstats.py
@@ -1,9 +1,11 @@
 import git
-from pathlib import Path
 import subprocess
+from pathlib import Path
+from collections import OrderedDict
+from datetime import datetime
 
 
-def is_not_git():
+def is_not_git() -> bool:
     """Check if git is initialized in the repository"""
     try:
         current_path = Path(__file__).parent.resolve()
@@ -13,11 +15,11 @@ def is_not_git():
         return True
 
 
-def get_logs(before="", after="", reverse=""):
+def get_logs(before="", after="", reverse="") -> list:
     """Return results of git log [args]"""
 
     # < day, date, author >
-    args = ["git", "log", "--pretty=format:%aD,%aN"]
+    args = ["git", "log", "--pretty=format:%ai,%aN"]
 
     commit_logs = []
     try:
@@ -32,14 +34,52 @@ def get_logs(before="", after="", reverse=""):
 
         # Process each line
         for line in logs:
-            day, time_stamp, author = line.split(",")
-            time_stamp = f"{day} -{time_stamp}"
+            time_stamp, author = line.split(",")
             commit_logs.append({"time_stamp": time_stamp, "author": author})
 
-        if(reverse):
+        if reverse:
             return commit_logs.reverse()
-            
+
         return commit_logs
 
     except Exception as e:
         print(e)
+
+
+def filter_logs(logs: list, author: str, frequency="day") -> dict:
+    """Filter the logs based on author and frequency(day, week, month, year)"""
+
+    commit_count_by_freq = OrderedDict()
+
+    for commit_info in logs:
+        commit_date = commit_info["time_stamp"].split(" ")[0]
+
+        is_weekend = False
+
+        if frequency == "week":
+            # %V gives number of weeks so far in a given year
+            commit_date = datetime.strptime(commit_date, "%Y-%m-%d").strftime("%Y/%V")
+        elif frequency == "month":
+            commit_date = commit_info["time_stamp"][:7]  # YYYY-MM
+        elif frequency == "year":
+            commit_date = commit_info["time_stamp"][:4]  # YYYY
+        else:
+            # return the day of the week
+            is_weekend = datetime.strptime(commit_date, "%Y-%m-%d").weekday() > 4
+            # Monday=0, Tuesday=1, Wednesday=2 and so on..
+
+        if author != "":
+            if author not in commit_info["author"]:
+                continue
+
+        if commit_date not in commit_count_by_freq:
+            # Add entry for that commit date
+            commit_count_by_freq[commit_date] = {
+                "time_stamp": commit_info["time_stamp"],
+                "commits": 0,
+                "weekend": is_weekend,
+            }
+
+        # Entry already exists for that commit date so increment
+        commit_count_by_freq[commit_date]["commits"] += 1
+    return commit_count_by_freq
--- main.py
+++ main.py
@@ -1,40 +1,22 @@
-import argparse
-
-from gitstats import is_not_git, get_logs
+from gitstats import is_not_git, get_logs, filter_logs, get_relative_count
+from parser import args
 
 
 def main():
     if is_not_git():
-        print('Git is not initialized. Please initialize using "git init"')
-
-    p = argparse.ArgumentParser(description="Display information from .git")
-
-    p.add_argument("-u", "--author", action="store", dest="author",
-                   type=str, required=False, default="",
-                   help="filter by author's e-mail (substring)")
-
-    p.add_argument("-a", "--after", action="store", dest="after",
-                   type=str, required=False, default="",
-                   help="after date (yyyy-mm-dd hh:mm)")
-
-    p.add_argument("-b", "--before", action="store", dest="before",
-                   type=str, required=False, default="",
-                   help="before date (yyyy-mm-dd hh:mm)")
-
-    p.add_argument("-r", "--reverse", action="store", dest="reverse",
-                   type=bool, required=False, default=False,
-                   help="reverse date order")
-    
-    args = p.parse_args()
+        print('Git is not initialized. Please initialize using \"git init\"')
 
     """Program start"""
-    items = []
+    logs = []
     try:
-        items = get_logs(args.after, args.before, args.reverse)
-        
+        logs = get_logs(args.after, args.before, args.reverse)
+        # for i in logs:
+        #     print(i)
     except Exception as e:
-        print("error running 'git log': %s" % (e,))
-        return
+        print(f"error running 'git log': {e}")
+        exit(1)
+
+    filtered = filter_logs(logs, args.author, args.frequency)
 
 if __name__ == "__main__":
     main()
