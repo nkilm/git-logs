e40fe476889c3b833ae4e63a51e4d519767980f8
Nikhil Mohite <nikhilmohitelhs@gmail.com>
2022-09-09 15:52:43 +0530

    add entry point

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
-import os
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-VERSION = '0.0.1'
-DESCRIPTION = 'Bird-eye view of a local git repository'
-LONG_DESCRIPTION = 'git-stats displays the statistics of a local git repository by listing all commits based on frequency, author etc'
+VERSION = "0.0.1"
+DESCRIPTION = "Bird-eye view of a local git repository"
+LONG_DESCRIPTION = "git-stats displays the statistics of a local git repository by listing all commits based on frequency, author etc"
 
 # Setting up
 setup(
@@ -16,15 +15,16 @@ setup(
     download_url="https://github.com/nkilm/git-logs",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    entry_points={
+    py_modules=["utils","validate"],
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+     entry_points={
         "console_scripts": [
             "git-logs = git-logs.main:main",
         ],
     },
-    long_description=LONG_DESCRIPTION,
-    packages=find_packages(),
-    install_requires=[],
-    keywords=['python', 'git','statistics','local repository','git statistics'],
+    keywords=["python", "git", "statistics", "local repository", "git statistics"],
     classifiers=[
         "Topic :: Utilities",
         "Intended Audience :: Developers",
@@ -33,5 +33,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Topic :: Software Development :: Version Control :: Git",
         "Programming Language :: Python :: 3",
-    ]
+    ],
 )
