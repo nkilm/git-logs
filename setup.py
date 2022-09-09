8f03fd0c8fe5b3af6d5a35e610217f22a458c557
Nikhil Mohite <nikhilmohitelhs@gmail.com>
2022-09-09 19:25:45 +0530

    v 0.0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import os
 import codecs
 
-VERSION = "0.0.1"
+VERSION = "0.0.3"
 DESCRIPTION = "Bird-eye view of a local git repository"
 
 here = os.path.abspath(os.path.dirname(__file__))
